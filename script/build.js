8a385f72248e9490e542b8e26c4dcf6a86f9606e
mnguyen <mnguyen@evenica.com>
2024-02-25 18:58:42 -0500

    add analyzer

--- script/build.js
+++ script/build.js
@@ -5,8 +5,24 @@ import BASE_OPTION from './.esbuildrc.js'
 main()
 
 function main() {
+  const { SOURCE_MAP } = process.env
+  const sourcemap = getOption(!!SOURCE_MAP, 'sourcemap', true)
   esbuild.build({
     ...BASE_OPTION,
+    ...sourcemap,
     minify: true,
   })
+}
+
+/**
+ *
+ * @template {import('esbuild').BuildOptions} T
+ * @template {keyof T} U
+ * @param {boolean} condition
+ * @param {U} key
+ * @param {T[U]} value
+ * @returns {{[key in U]: T[U]}}
+ */
+function getOption(condition, key, value) {
+  return condition ? { [key]: value } : {}
 }
